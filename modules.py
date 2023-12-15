8e25e6889704e624fe712577c9064a77dac7ea24
hamzaaitbrik <kirbtia@gmail.com>
2023-12-15 15:50:15 +0100

    adding modules

--- modules.py
+++ modules.py
@@ -1,2 +1,8 @@
-from time import sleep
 from cv2 import VideoCapture, imwrite
+from time import sleep
+from datetime import datetime
+
+def log(data):
+        print(f'[{str(datetime.now().strftime(r"%Y-%m-%d %H:%M:%S"))}] - {data}')
+        with open('log.txt', 'a') as log:
+            log.write(f'[{str(datetime.now().strftime(r"%Y-%m-%d %H:%M:%S"))}] - {data}\n')
