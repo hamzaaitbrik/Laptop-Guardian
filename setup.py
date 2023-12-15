aa1343c903a67d665cad87e7bd32f8f271332647
hamzaaitbrik <kirbtia@gmail.com>
2023-12-15 16:25:37 +0100

    further development

---
+++ setup.py
@@ -0,0 +1,15 @@
+from modules import log
+
+
+
+def getInfos():
+    def getOS()
+        log('[SETUP] What operating system are you running? Please, insert W for Windows and L for Linux.')
+        OS = input()
+        while(OS not in ['L', 'W']):
+            getOS()
+        print(OS)
+
+
+
+getInfos()
