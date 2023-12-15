c46c1909d4fb4d408f1515bd4c82a433b6704897
hamzaaitbrik <kirbtia@gmail.com>
2023-12-15 20:57:56 +0100

    finalizing

--- modules.py
+++ modules.py
@@ -3,7 +3,7 @@ from asyncio import get_event_loop
 from telegram import Bot
 from time import sleep
 from datetime import datetime
-from os import mkdir, getcwd, chdir
+from os import mkdir, getcwd
 from config import config
 
 def log(data):
